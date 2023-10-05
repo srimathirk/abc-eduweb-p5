f06f7ce90446af646fa3164cab197c07a436e18f
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-10-05 16:14:40 -0400

    updating apppy

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request,session
 from flask_restful import Resource
 
 # Local imports
@@ -13,6 +13,96 @@ from models import User
 
 
 # Views go here!
+class ClearSession(Resource):
+
+    def delete(self):
+    
+        session['page_views'] = None
+        session['user_id'] = None
+
+        return {}, 204
+
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        print(f"User ID from session: {user_id}")
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            print(f"User from database: {user}")
+            if user:           
+                return user.to_dict(), 200
+        else:
+            session.pop('user_id', None)
+            return {}, 204
+
+
+
+class Signup(Resource):
+    
+    def post(self):
+        username = request.get_json()['username']
+        # user = User.query.filter(User.username == username)
+
+        password = request.get_json()['password']
+        if username and password:
+            new_user = User(
+                username=username
+            )
+            new_user.password_hash = password
+
+            db.session.add(new_user)
+            db.session.commit()
+
+            session['user_id'] = new_user.id
+            print(session)
+            return new_user.to_dict(), 201
+        else:
+            return {},204
+    
+        # return {'error': 'Invalid username or password'}, 401
+
+        
+
+class Login(Resource):
+    def post(self):
+        
+        username = request.get_json()['username']
+        
+        password = request.get_json()['password']
+        user = User.query.filter(User.username == username).first()
+
+        # users pwd is set by calling user.pawd = "new_pwd"
+        # instead of pwd = user.pwd , here we authenticate by using bcrypt checking pwd = stored pwd hash
+        if user and user.authenticate(password):
+        
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+
+        return {}, 401
+ 
+
+class Logout(Resource):
+    def delete(self):
+
+        # session['user_id'] = None
+        session.pop ('user_id', None)
+        return {}, 204
+
+
+#RESTful route syntax
+class Users(Resource):
+    def get(self):
+        users = [user.to_dict() for user in User.query.all()] #Serialize users - password hashes shouldnot be sent to client  
+        return users, 200
+
+
+api.add_resource(ClearSession, '/clear', endpoint='clear')
+api.add_resource(CheckSession, '/check_session', endpoint='checksession')
+api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+api.add_resource(Users, '/users')
 
 @app.route('/')
 def index():
