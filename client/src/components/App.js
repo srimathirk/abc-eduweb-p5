c7ab5e7b08fc6117b78dcade97603b060248470d
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-10-05 16:12:36 -0400

    updating app.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,71 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import SignUp from "./SignUp";
+import Login from "./Login";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import Books from "./Books";
+import MathQuiz from './MathQuiz';
+import Logic from "./Logic";
 
 function App() {
-  return <h1>Project Client</h1>;
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    console.log("Chcking session works");
+    fetch("/check_session")
+      .then((r) => {
+        if (r.ok) {
+          return r.text(); // Get the response as text
+        }
+      })
+      .then((text) => {
+        console.log("Response Text:", text);
+        // Now try to parse it as JSON if it's not empty
+        if (text.trim() !== "") {
+          const user = JSON.parse(text); //converting text to JSON
+          setUser(user);
+          console.log(user);
+        }
+      });
+  }, []);
+  console.log(user)
+  return (
+    <>
+      <NavBar user={user} setUser={setUser} />
+      <main>
+        {user ? (
+          <Switch>
+            <Route path="/">
+              <Home user={user} />
+            </Route>
+            <Route path="/books">
+              <Books user={user}/>
+            </Route>
+            <Route path="/mathquiz">
+              <MathQuiz user={user}/>
+            </Route>
+            <Route path="/logic">
+              <Logic />
+            </Route>
+          </Switch>
+        ) : (
+          <Switch>
+            <Route path="/signup">
+              <SignUp setUser={setUser} />
+            </Route>
+            <Route path="/login">
+              <Login setUser={setUser} />
+            </Route>
+            <Route path="/">
+              <Home user={user}/>
+            </Route>
+            
+          </Switch>
+        )}
+      </main>
+    </>
+  );
 }
 
 export default App;
