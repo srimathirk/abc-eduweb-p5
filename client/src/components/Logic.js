6c5df7a56222d6526c1f770b03bdd28fb68cf992
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-10-05 16:13:09 -0400

    updating logic.js

---
+++ client/src/components/Logic.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Logic() {
+  return (
+    <div>
+      <h2>Logic Component</h2>
+      {/* Add content for Logic here */}
+    </div>
+  );
+}
+
+export default Logic;
