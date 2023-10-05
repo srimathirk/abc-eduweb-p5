2c272660094e61ceb51ac25a2e1124a6dfcdaed9
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-10-05 16:12:11 -0400

    updating index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,12 @@
-import React from "react";
-import App from "./components/App";
-import "./index.css";
-import { createRoot } from "react-dom/client";
+// index.js or App.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './components/App.js';
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
