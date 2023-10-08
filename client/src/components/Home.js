890ef33ff9b00188d0f3685151bbde953ff76146
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-10-08 18:46:36 -0400

    updating home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,44 +1,42 @@
-import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
-import Books from './Books'; // Import your Books, Math, and Logic components
-import MathQuiz from './MathQuiz';
-import Logic from './Logic';
-import '../css/Home.css'
-
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import Books from "./Books"; // Import your Books, Math, and Logic components
+import MathQuiz from "./MathQuiz";
+import "../css/Home.css";
 
 function Home({ user }) {
-  console.log(user)
-  if (user){
-  return (
-    <div  >
-      <h1>Welcome to the Home {user.username}!</h1>
+  console.log(user);
+  if (user) {
+    return (
+      <div>
+        <h1>Welcome to the Home {user.username}!</h1>
 
-      <nav>
-        <ul>
-          {user && <li>
-            <Link to="/books">Books</Link>
-          </li>}
-          <li>
-            <Link to="/mathquiz">MathQuiz</Link>
-          </li>
-          <li>
-            <Link to="/logic">Logic</Link>
-          </li>
-        </ul>
-      </nav>
-      <Switch>
-        <Route path="/books"> <Books user={user}/></Route> 
-        <Route path="/mathquiz">
-              <MathQuiz user={user}/>
-            </Route>
-        <Route path="/logic" component={Logic} />
-      </Switch>
-    </div>
-  );
-}
-else {
-  return <h1 className="bg">Please Login or Sign Up</h1>;
-}
+        <nav>
+          <ul>
+            {user && (
+              <li>
+                <Link to="/books">Books</Link>
+              </li>
+            )}
+            <li>
+              <Link to="/mathquiz">MathQuiz</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/books">
+            {" "}
+            <Books user={user} />
+          </Route>
+          <Route path="/mathquiz">
+            <MathQuiz user={user} />
+          </Route>
+        </Switch>
+      </div>
+    );
+  } else {
+    return <h1 className="bg">Please Login or Sign Up</h1>;
+  }
 }
 
 // function Books() {
@@ -53,21 +51,19 @@ else {
 //   return <h2>Logic Component</h2>;
 // }
 
-export default Home
-
+export default Home;
 
 // function Home({ user }) {
 //     if (user) {
 //       return (
 //       <div>
 //       <h1>Welcome, {user.username} !</h1>
-      
+
 //     </div>
 //       )
 //     } else {
 //       return <h1>Please Login or Sign Up</h1>;
 //     }
 //   }
-  
+
 //   export default Home;
-  
